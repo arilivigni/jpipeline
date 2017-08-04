4c08c6f162cdcdc6492deda28137355fb5fafaa6
Ari LiVigni <alivigni@redhat.com>
2017-08-04 17:28:12 -0400

    Add a utils with allocDuffy

---
+++ src/utils.groovy
@@ -0,0 +1,21 @@
+def allocDuffy(Stage) {
+    //echo "Currently in stage: ${stage} ${env.DUFFY_OP} resources"
+    echo "Currently in stage: ${stage} resources"
+    env.ORIGIN_WORKSPACE = "${env.WORKSPACE}/${stage}"
+    env.ORIGIN_BUILD_TAG = "${env.BUILD_TAG}-${stage}"
+    env.ORIGIN_CLASS = "builder"
+    env.DUFFY_JOB_TIMEOUT_SECS = "3600"
+
+    //withCredentials([file(credentialsId: 'duffy-key', variable: 'DUFFY_KEY')]) {
+        sh '''
+            #!/bin/bash
+            set -xeuo pipefail
+    
+            echo "HELLO ${JOB_NAME}"
+            exit
+        '''
+   // }
+}
+
+return this
+
