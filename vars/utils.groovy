090a0c9f78e5b33b6f06ff45a1638060d500afea
Ari LiVigni <alivigni@redhat.com>
2017-08-04 14:16:11 -0400

    Add sample acme library

---
+++ vars/utils.groovy
@@ -0,0 +1,12 @@
+class utils implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
