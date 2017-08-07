8b3d21fb51f2ffcd13890c91befdbb1b9185d3dc
Ari LiVigni <alivigni@redhat.com>
2017-08-07 12:40:23 -0400

    Experiment with env inside call

--- vars/rpmBuild.groovy
+++ vars/rpmBuild.groovy
@@ -8,6 +8,7 @@ def call(body) {
     //node {
     // Clean workspace before doing anything
     // deleteDir()
+    echo "${env.JOB_NAME}"
     try {
         def currentStage = 'ci-pipeline-rpmbuild'
         stage (currentStage) {
