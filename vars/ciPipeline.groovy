d548b156376ad54cc6bb7b0f413e8771d604c18c
Ari LiVigni <alivigni@redhat.com>
2017-08-09 11:55:55 -0400

    Fix the current stage

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -15,9 +15,9 @@ def call(body) {
             sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
             sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
             env.DUFFY_OPS = "--allocate"
-            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            getDuffy.duffy(current_stage, "${env.DUFFY_OPS}")
             env.DUFFY_OPS = "--teardown"
-            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            getDuffy.duffy(current_stage, "${env.DUFFY_OPS}")
 
         }
         current_stage = 'ci-pipeline-ostree-compose'
@@ -26,9 +26,9 @@ def call(body) {
             sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
             sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
             env.DUFFY_OPS = "--allocate"
-            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            getDuffy.duffy(current_stage, "${env.DUFFY_OPS}")
             env.DUFFY_OPS = "--teardown"
-            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            getDuffy.duffy(current_stage, "${env.DUFFY_OPS}")
         }
     } catch (err) {
         echo "Error: Exception from " + current_stage + ":"
