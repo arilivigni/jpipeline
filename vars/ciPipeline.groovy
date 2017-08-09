8bed14848411942c3294ef8953d39053df254413
Ari LiVigni <alivigni@redhat.com>
2017-08-09 11:41:28 -0400

    Jenkins-sl-pipeline is to call ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -8,36 +8,31 @@ def call(body) {
     body()
 
     def getDuffy = new duffy()
-    node('shared-lib-test') {
-        deleteDir()
+    try {
+        def current_stage = 'ci-pipeline-rpmbuild'
+        stage(current_stage) {
+            echo "Our main topic is ${env.MAIN_TOPIC}"
+            sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
+            sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
+            env.DUFFY_OPS = "--allocate"
+            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            env.DUFFY_OPS = "--teardown"
+            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
 
-        try {
-            def current_stage = 'ci-pipeline-rpmbuild'
-            stage(current_stage) {
-                echo "Our main topic is ${env.MAIN_TOPIC}"
-                sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
-                sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
-                env.DUFFY_OPS = "--allocate"
-                getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
-                env.DUFFY_OPS = "--teardown"
-                getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
-
-            }
-            current_stage = 'ci-pipeline-ostree-compose'
-            stage(current_stage) {
-                echo "Our main topic is ${env.MAIN_TOPIC}"
-                sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
-                sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
-                env.DUFFY_OPS = "--allocate"
-                getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
-                env.DUFFY_OPS = "--teardown"
-                getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
-            }
-        } catch (err) {
-            echo "Error: Exception from " + current_stage + ":"
-            echo e.getMessage()
-            throw err
         }
-
+        current_stage = 'ci-pipeline-ostree-compose'
+        stage(current_stage) {
+            echo "Our main topic is ${env.MAIN_TOPIC}"
+            sh "echo 'rpmmbuild building on branch ${env.TARGET_BRANCH} ...'"
+            sh "echo 'Project Repo is ${env.PROJECT_REPO}...'"
+            env.DUFFY_OPS = "--allocate"
+            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+            env.DUFFY_OPS = "--teardown"
+            getDuffy.duffy(currentStage, "${env.DUFFY_OPS}")
+        }
+    } catch (err) {
+        echo "Error: Exception from " + current_stage + ":"
+        echo e.getMessage()
+        throw err
     }
 }
