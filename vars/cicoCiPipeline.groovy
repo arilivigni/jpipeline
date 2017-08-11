eb3575b25f2cc83645202fe170735a174406b5db
Ari LiVigni <alivigni@redhat.com>
2017-08-11 16:37:49 -0400

    Put back current_stage since there is another issue

--- vars/cicoCiPipeline.groovy
+++ vars/cicoCiPipeline.groovy
@@ -10,18 +10,15 @@ def call(body) {
     def getDuffy = new Utils()
     try {
         def current_stage = 'cico-pipeline-lib-stage1'
-        //stage(current_stage) {
-        stage('cico-pipeline-lib-stage1') {
-            //getDuffy.duffyCciskel(current_stage)
-            getDuffy.duffyCciskel('cico-pipeline-lib-stage1')
+        stage(current_stage) {
+            getDuffy.duffyCciskel(current_stage)
         }
         current_stage = 'cico-pipeline-lib-stage2'
         stage(current_stage) {
-            //getDuffy.duffyCciskel(current_stage)
-            getDuffy.duffyCciskel('cico-pipeline-lib-stage1')
+            getDuffy.duffyCciskel(current_stage)
         }
     } catch (err) {
-        //echo "Error: Exception from " + current_stage + ":"
+        echo "Error: Exception from " + current_stage + ":"
         echo e.getMessage()
         throw err
     }
