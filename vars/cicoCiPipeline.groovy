c290debdbf6e2a08256bf25dccbd0f8f82753aa0
Ari LiVigni <alivigni@redhat.com>
2017-08-11 16:35:01 -0400

    Take current_stage out of the call to root cause

--- vars/cicoCiPipeline.groovy
+++ vars/cicoCiPipeline.groovy
@@ -10,16 +10,18 @@ def call(body) {
     def getDuffy = new Utils()
     try {
         def current_stage = 'cico-pipeline-lib-stage1'
-        stage(current_stage) {
-            getDuffy.duffyCciskel(current_stage)
-
+        //stage(current_stage) {
+        stage('cico-pipeline-lib-stage1') {
+            //getDuffy.duffyCciskel(current_stage)
+            getDuffy.duffyCciskel('cico-pipeline-lib-stage1')
         }
         current_stage = 'cico-pipeline-lib-stage2'
         stage(current_stage) {
-            getDuffy.duffyCciskel(current_stage)
+            //getDuffy.duffyCciskel(current_stage)
+            getDuffy.duffyCciskel('cico-pipeline-lib-stage1')
         }
     } catch (err) {
-        echo "Error: Exception from " + current_stage + ":"
+        //echo "Error: Exception from " + current_stage + ":"
         echo e.getMessage()
         throw err
     }
